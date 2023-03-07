e8fa2d69fa6fdca763161dea735340c37e2082fd
Miguel Mendoza <mendozamiguel7n@gmail.com>
2023-03-06 20:08:11 -0500

    Se crea la web app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the SkateShop
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>
+          Miguel Mendoza
+        </small>
       </header>
     </div>
   );
